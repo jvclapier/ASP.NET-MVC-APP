23359692130f72af477bfff4ffdd8690684e4834
jvclapier <jvclapier@gmail.com>
2016-10-17 10:58:41 -0600

    BlowOut App Checkpoint 1

---
+++ BlowOut/BlowOut/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlowOut.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public string Index()
+        {
+            
+            return "Please call Support at <b><u>801-555-1212.</u></b> Thank you!";
+        }
+
+        public ActionResult Email(string name, string email)
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+
+            return View();
+        }
+    }
+}
